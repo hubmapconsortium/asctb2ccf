d1e9a46f9ee1b918b446502e17d4a364ac31a5b2
Josef Hardi <josef.hardi@gmail.com>
2023-06-12 11:05:43 -0700

    Output an empty ontology when Google Sheets URL is empty

--- asctb2ccf/pipeline.py
+++ asctb2ccf/pipeline.py
@@ -17,25 +17,26 @@ def run(args):
 
     o = BSOntology.new(organ_name, ontology_iri)
 
-    if args.cell_biomarkers_only:
-        response = client.get_data_by_gsheet_url(gsheet_url)
-        for index, data_item in enumerate(response['data']):
-            try:
-                o = o.mutate_cell_biomarker(data_item)
-            except ValueError as e:
-                logging.warning(str(e) +
-                    f", row {index}, in <spreadsheet> {organ_name}")
-    else:
-        response = client.get_data_by_gsheet_url(gsheet_url)
-        for index, data_item in enumerate(response['data']):
-            try:
-                o = o.mutate_anatomical_structure(data_item)
-                o = o.mutate_cell_type(data_item)
-                o = o.mutate_biomarker(data_item)
-                o = o.mutate_partonomy(data_item)
-                o = o.mutate_cell_hierarchy(data_item)
-                o = o.mutate_cell_location(data_item)
-            except ValueError as e:
-                logging.warning(str(e) +
-                    f", row {index}, in <spreadsheet> {organ_name}")
+    if (gsheet_url):
+        if args.cell_biomarkers_only:
+            response = client.get_data_by_gsheet_url(gsheet_url)
+            for index, data_item in enumerate(response['data']):
+                try:
+                    o = o.mutate_cell_biomarker(data_item)
+                except ValueError as e:
+                    logging.warning(str(e) +
+                        f", row {index}, in <spreadsheet> {organ_name}")
+        else:
+            response = client.get_data_by_gsheet_url(gsheet_url)
+            for index, data_item in enumerate(response['data']):
+                try:
+                    o = o.mutate_anatomical_structure(data_item)
+                    o = o.mutate_cell_type(data_item)
+                    o = o.mutate_biomarker(data_item)
+                    o = o.mutate_partonomy(data_item)
+                    o = o.mutate_cell_hierarchy(data_item)
+                    o = o.mutate_cell_location(data_item)
+                except ValueError as e:
+                    logging.warning(str(e) +
+                        f", row {index}, in <spreadsheet> {organ_name}")
     o.serialize(args.output)
